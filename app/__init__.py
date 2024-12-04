4414d328a8cf0bebfe329e29301ec90f765af50d
Nikki Vaughan <vnikki13@gmail.com>
2024-12-04 12:37:07 -0500

    Finish lesson 1: Create end point to read all books

---
+++ app/__init__.py
@@ -0,0 +1,9 @@
+from flask import Flask
+from .routes.book_routes import books_bp
+
+def create_app():
+    app = Flask(__name__)
+
+    # Register Blueprints here
+    app.register_blueprint(books_bp)
+    return app
