ab471a17e46ed1caf4f8ad7d0d22464ee4b21eba
Nikki Vaughan <vnikki13@gmail.com>
2025-01-23 15:28:46 -0500

    Finish lesson 3: Database setup and create and read data.

--- app/routes/book_routes.py
+++ app/routes/book_routes.py
@@ -1,41 +1,36 @@
-from flask import Blueprint, abort, make_response
-from app.models.book import books
+from flask import Blueprint, abort, make_response, request
+from app.models.book import Book
+from ..db import db
 
 books_bp = Blueprint("books_bp", __name__, url_prefix="/books")
 
 @books_bp.get("")
 def get_all_books():
-    books_response = []
-    for book in books:
-        books_response.append(
-            {
-                "id": book.id,
-                "title": book.title,
-                "description": book.description
-            }
-        )
-    return books_response
+  query = db.select(Book).order_by(Book.id)
+  books = db.session.scalars(query)
+  
+  books_response = []
+  for book in books:
+    books_response.append(
+      {"id": book.id, "title": book.title, "description": book.description}
+    )
 
-@books_bp.get("/<book_id>")
-def get_one_book(book_id):
-    book = validate_book(book_id)
+  return books_response  
 
-    return {
-        "id": book.id,
-        "title": book.title,
-        "description": book.description,
-    }
+@books_bp.post("")
+def create_book():
+  request_body = request.get_json()
+  title = request_body["title"]
+  description = request_body["description"]
 
-def validate_book(book_id):
-    try:
-        book_id = int(book_id)
-    except:
-        response = {"message": f"book {book_id} invalid"}
-        abort(make_response(response, 400))
+  new_book = Book(title=title, description=description)
+  db.session.add(new_book)
+  db.session.commit()
 
-    for book in books:
-        if book.id == book_id:
-            return book
+  response = {
+    "id": new_book.id,
+    "title": new_book.title,
+    "description": new_book.description,
+  }
 
-    response = {"message": f"book {book_id} not found"}
-    abort(make_response(response, 404))
+  return response, 201
