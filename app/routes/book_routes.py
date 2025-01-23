f691fa1dcb1e60530269d67aaf41b190e3f8b171
Nikki Vaughan <vnikki13@gmail.com>
2025-01-23 17:21:56 -0500

    Finish lesson 4: Create, Read and Delete a book

--- app/routes/book_routes.py
+++ app/routes/book_routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, abort, make_response, request
+from flask import Blueprint, abort, make_response, request, Response
 from app.models.book import Book
 from ..db import db
 
@@ -15,7 +15,28 @@ def get_all_books():
       {"id": book.id, "title": book.title, "description": book.description}
     )
 
-  return books_response  
+  return books_response
+
+@books_bp.get("/<book_id>")
+def get_one_book(book_id):
+  book = validate_book(book_id)
+
+  return {
+    "id": book.id,
+    "title": book.title,
+    "description": book.description,
+  }
+
+@books_bp.put("/<book_id>")
+def update_book(book_id):
+  book = validate_book(book_id)
+  request_body = request.get_json()
+
+  book.title = request_body["title"]
+  book.description = request_body["description"]
+  db.session.commit()
+
+  return Response(status=204, mimetype="application/json")
 
 @books_bp.post("")
 def create_book():
@@ -34,3 +55,27 @@ def create_book():
   }
 
   return response, 201
+
+@books_bp.delete("/<book_id>")
+def delete_book(book_id):
+  book = validate_book(book_id)
+  db.session.delete(book)
+  db.session.commit()
+  
+  return Response(status=204, mimetype="application/json")
+
+def validate_book(book_id):
+  try:
+      book_id = int(book_id)
+  except:
+      response = {"message": f"book {book_id} invalid"}
+      abort(make_response(response , 400))
+
+  query = db.select(Book).where(Book.id == book_id)
+  book = db.session.scalar(query)
+  
+  if not book:
+    response = {"message": f"book {book_id} not found"}
+    abort(make_response(response, 404))
+
+  return book
