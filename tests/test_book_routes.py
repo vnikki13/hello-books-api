cc0df38c19447b9c1f5ff241933d6638a8feb24d
Nikki Vaughan <vnikki13@gmail.com>
2025-01-30 13:45:50 -0500

    Finish lesson 6: Testing

---
+++ tests/test_book_routes.py
@@ -0,0 +1,53 @@
+def test_get_all_books_with_no_records(client):
+    # Act
+    response = client.get("/books")
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 200
+    assert response_body == []
+
+def test_get_all_books_with_two_records(client, two_saved_books):
+    # Act
+    response = client.get("/books")
+    response_body = response.get_json()
+
+    # Assert
+    response.status_code == 200
+    assert len(response_body) == 2
+
+def test_get_one_book(client, two_saved_books):
+    # Act
+    response = client.get("/books/1")
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 200
+    assert response_body == {
+        "id": 1,
+        "title": "Ocean Book",
+        "description": "watr 4evr"
+    }
+
+def test_get_nonexisting_book(client):
+    # Act
+    response = client.get("/books/100")
+
+    # Assert
+    assert response.status_code == 404
+
+def test_create_one_book(client):
+    # Act
+    response = client.post("/books", json={
+        "title": "New Book",
+        "description": "The Best!"
+    })
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 201
+    assert response_body == {
+        "id": 1,
+        "title": "New Book",
+        "description": "The Best!"
+    }
